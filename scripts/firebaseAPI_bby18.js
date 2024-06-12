130e131ca8b0698e675aa60e324fc8acef99ef3a
yiming168 <96162256+yiming168@users.noreply.github.com>
2024-06-12 01:25:30 -0700

    Update firebaseAPI_bby18.js

--- scripts/firebaseAPI_bby18.js
+++ scripts/firebaseAPI_bby18.js
@@ -2,12 +2,12 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 const firebaseConfig = {
-  apiKey: "AIzaSyBy4gSQNpxx8wMcKkEDBRKoMxAUTbtBgmg",
-  authDomain: "bby18-6af8d.firebaseapp.com",
-  projectId: "bby18-6af8d",
-  storageBucket: "bby18-6af8d.appspot.com",
-  messagingSenderId: "1015274578073",
-  appId: "1:1015274578073:web:49a920e799d5faf5901808"
+apiKey: "AIzaSyC16cCS-v8sJe6b6TfHo3bDVtMKsaqc0r8",
+  authDomain: "comp1800-20240220-demo.firebaseapp.com",
+  projectId: "comp1800-20240220-demo",
+  storageBucket: "comp1800-20240220-demo.appspot.com",
+  messagingSenderId: "701735076076",
+  appId: "1:701735076076:web:bc304674179407a223e5c9"
 };
 
 //--------------------------------------------
